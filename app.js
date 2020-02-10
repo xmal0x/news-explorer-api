b23019911777fe6bd2f1144c24583df285fdab10
Kirill Galkin <malo2dis@gmail.com>
2020-02-10 21:15:47 +0300

    fix lint

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
 
   res
     .status(statusCode)
-    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка:' + message : message });
+    .send({ message: statusCode === 500 ? 'На сервере произошла ошибка:' : message });
 });
 
 app.listen(PORT, () => {
