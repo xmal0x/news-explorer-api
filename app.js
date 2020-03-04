2e5ce8c0d6913778083828728c72b07baa503f5f
Kirill Galkin <malo2dis@gmail.com>
2020-03-04 20:34:35 +0300

    fix cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://api-news.ga/');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
